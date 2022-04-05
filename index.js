797959c7cd8f8227904005826da822534925bcb9
Windwalk <windwalk-bushido@protonmail.com>
2022-04-05 11:40:41 +0200

    Formatted code properly

--- index.js
+++ index.js
@@ -12,221 +12,6 @@ const main = () => {
     this.evaluation_points = 0;
   }
 
-  let goals_for_team_1 = null;
-  let goals_for_team_2 = null;
-
-  function GetGoals() {
-    function RandomNumber() {
-      const min = 0;
-      const max = 8; // Ne zelim da vredjam nijedan klub ovim ali ovo je samo zbog jednostavnosti...
-      return Math.floor(Math.random() * (max - min) + min);
-    }
-
-    goals_for_team_1 = RandomNumber();
-    goals_for_team_2 = RandomNumber();
-  }
-
-  function SimulateMatch(team1, team2) {
-    team1.goals_given += goals_for_team_1;
-    team1.goals_received += goals_for_team_2;
-    team2.goals_given += goals_for_team_2;
-    team2.goals_received += goals_for_team_1;
-
-    if (goals_for_team_1 > goals_for_team_2) {
-      team1.points += 3;
-      team1.wins += 1;
-      team2.loses += 1;
-    } else if (goals_for_team_1 === goals_for_team_2) {
-      team1.points += 1;
-      team1.draws += 1;
-      team2.points += 1;
-      team2.draws += 1;
-    } else {
-      team2.points += 3;
-      team2.wins += 1;
-      team1.loses += 1;
-    }
-  }
-
-  function PrintGroupMatchResult(team1, team2) {
-    console.log("\t\t" + team1 + "\t" + goals_for_team_1 + ":" + goals_for_team_2 + "\t" + team2);
-  }
-
-  function SimulateGroupMatches(group, round) {
-    console.log("\tGrupa " + group[0].group + ":");
-    switch (round) {
-      case 0:
-        GetGoals();
-        SimulateMatch(group[0], group[1]);
-        PrintGroupMatchResult(group[0].name, group[1].name);
-
-        GetGoals();
-        SimulateMatch(group[2], group[3]);
-        PrintGroupMatchResult(group[2].name, group[3].name);
-        break;
-      case 1:
-        GetGoals();
-        SimulateMatch(group[0], group[3]);
-        PrintGroupMatchResult(group[0].name, group[3].name);
-
-        GetGoals();
-        SimulateMatch(group[1], group[2]);
-        PrintGroupMatchResult(group[1].name, group[2].name);
-        break;
-      case 2:
-        GetGoals();
-        SimulateMatch(group[0], group[2]);
-        PrintGroupMatchResult(group[0].name, group[2].name);
-
-        GetGoals();
-        SimulateMatch(group[1], group[3]);
-        PrintGroupMatchResult(group[1].name, group[3].name);
-        break;
-    }
-    console.log("\n");
-  }
-
-  function PrintGroupResults(group) {
-    let goals_given = "";
-    let goals_received = "";
-    for (let i = 0; i < group.length; i++) {
-      // Zbog boljeg formatiranja
-      if (group[i].goals_given < 10) {
-        goals_given = "0" + group[i].goals_given.toString();
-      } else {
-        goals_given = group[i].goals_given;
-      }
-      if (group[i].goals_received < 10) {
-        goals_received = "0" + group[i].goals_received.toString();
-      } else {
-        goals_received = group[i].goals_received;
-      }
-
-      console.log(
-        "\t\t" +
-          (i + 1) +
-          ". " +
-          group[i].name +
-          " (" +
-          group[i].rank +
-          ")\t" +
-          group[i].wins +
-          "  " +
-          group[i].draws +
-          "  " +
-          group[i].loses +
-          "  " +
-          goals_given +
-          ":" +
-          goals_received +
-          "  " +
-          group[i].points
-      );
-    }
-  }
-
-  function SortArray(array) {
-    return array.sort(function (a, b) {
-      return b - a;
-    });
-  }
-
-  function FindTeam(group, array_element) {
-    for (let i = 0; i < group.length; i++) {
-      if (array_element === group[i].evaluation_points) {
-        return group[i];
-      }
-    }
-  }
-
-  function CalculateEvaluationPoints(group) {
-    let temp_array = [];
-    for (let i = 0; i < group.length; i++) {
-      group[i].evaluation_points =
-        group[i].points * 16 +
-        (group[i].goals_given - group[i].goals_received) * 2 +
-        group[i].goals_given * 0.25 +
-        group[i].rank * -0.03125;
-      if (group[i].evaluation_points < 0) {
-        group[i].evaluation_points = (group[i].evaluation_points * -1) / 100;
-      }
-      temp_array.unshift(group[i].evaluation_points);
-    }
-    return temp_array;
-  }
-
-  function PickGroupWinners(group, sorted_list) {
-    let group_winners = [];
-    group_winners[0] = FindTeam(group, sorted_list[0]);
-    group_winners[1] = FindTeam(group, sorted_list[1]);
-    return group_winners;
-  }
-
-  function ShiftGroup(group, sorted_list) {
-    let shifted_group = [];
-    for (let i = 0; i < group.length; i++) {
-      for (let y = 0; y < group.length; y++) {
-        if (sorted_list[i] === group[y].evaluation_points) {
-          shifted_group[i] = group[y];
-        }
-      }
-    }
-    return shifted_group;
-  }
-
-  function RoundupResults(group, winners_group) {
-    list_of_eval_points = CalculateEvaluationPoints(group);
-    sorted_list = SortArray(list_of_eval_points);
-
-    group = ShiftGroup(group, sorted_list);
-    winners_group = PickGroupWinners(group, sorted_list);
-
-    // Zbog boljeg formatiranja
-    if (group[0].group === "A") {
-      console.log("\n\tGrupa " + group[0].group);
-    } else {
-      console.log("\n\n\tGrupa " + group[0].group);
-    }
-    PrintGroupResults(group);
-
-    winners_group[0].group += "1";
-    winners_group[1].group += "2";
-    return winners_group;
-  }
-
-  function PrintMatchResult(team1, team2) {
-    console.log(
-      "\t(" +
-        team1.group +
-        ") " +
-        team1.name +
-        "\t" +
-        goals_for_team_1 +
-        ":" +
-        goals_for_team_2 +
-        "\t" +
-        team2.name +
-        " (" +
-        team2.group +
-        ")"
-    );
-  }
-
-  function SimulateGame(team1, team2) {
-    do {
-      GetGoals();
-    } while (goals_for_team_1 === goals_for_team_2);
-    PrintMatchResult(team1, team2);
-
-    if (goals_for_team_1 > goals_for_team_2) {
-      return team1;
-    } else {
-      return team2;
-    }
-  }
-
-  // MAIN APP
-
   // Zbog boljeg formatiranja imena imaju razmak u imenu
   const Katar = new Team("Katar            ", 51, "A");
   const Ekvador = new Team("Ekvador          ", 46, "A");
@@ -276,40 +61,92 @@ const main = () => {
   const Juzna_Koreja = new Team("Juzna Koreja     ", 29, "H");
   let group_H = [Portugal, Gana, Urugvaj, Juzna_Koreja];
 
+  let goals_for_team_1 = null;
+  let goals_for_team_2 = null;
+
+  function GetGoals() {
+    function RandomNumber() {
+      const min = 0;
+      const max = 8; // Ne zelim da vredjam nijedan tim ovim ali ovo je samo zbog jednostavnosti...
+      return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    goals_for_team_1 = RandomNumber();
+    goals_for_team_2 = RandomNumber();
+  }
+
+  function PrintGroupMatchResult(team1, team2) {
+    console.log("\t\t" + team1 + "\t" + goals_for_team_1 + ":" + goals_for_team_2 + "\t" + team2);
+  }
+
+  function SimulateGroupMatch(team1, team2) {
+    GetGoals();
+    team1.goals_given += goals_for_team_1;
+    team1.goals_received += goals_for_team_2;
+    team2.goals_given += goals_for_team_2;
+    team2.goals_received += goals_for_team_1;
+
+    if (goals_for_team_1 > goals_for_team_2) {
+      team1.points += 3;
+      team1.wins += 1;
+      team2.loses += 1;
+    } else if (goals_for_team_1 === goals_for_team_2) {
+      team1.points += 1;
+      team1.draws += 1;
+      team2.points += 1;
+      team2.draws += 1;
+    } else {
+      team2.points += 3;
+      team2.wins += 1;
+      team1.loses += 1;
+    }
+
+    PrintGroupMatchResult(team1.name, team2.name);
+  }
+
+  function SimulateGroupTournament(group, round) {
+    console.log("\tGrupa " + group[0].group + ":");
+    switch (round) {
+      case 0:
+        SimulateGroupMatch(group[0], group[1]);
+        SimulateGroupMatch(group[2], group[3]);
+        break;
+      case 1:
+        SimulateGroupMatch(group[0], group[3]);
+        SimulateGroupMatch(group[1], group[2]);
+        break;
+      case 2:
+        SimulateGroupMatch(group[0], group[2]);
+        SimulateGroupMatch(group[1], group[3]);
+        break;
+    }
+    console.log("\n");
+  }
+
+  function SimulateGroupEliminationPhase(round) {
+    SimulateGroupTournament(group_A, round);
+    SimulateGroupTournament(group_B, round);
+    SimulateGroupTournament(group_C, round);
+    SimulateGroupTournament(group_D, round);
+    SimulateGroupTournament(group_E, round);
+    SimulateGroupTournament(group_F, round);
+    SimulateGroupTournament(group_G, round);
+    SimulateGroupTournament(group_H, round);
+  }
+
   for (let i = 0; i < 3; i++) {
     switch (i) {
       case 0:
         console.log("\nGrupna faza - I kolo:\n");
-        SimulateGroupMatches(group_A, i);
-        SimulateGroupMatches(group_B, i);
-        SimulateGroupMatches(group_C, i);
-        SimulateGroupMatches(group_D, i);
-        SimulateGroupMatches(group_E, i);
-        SimulateGroupMatches(group_F, i);
-        SimulateGroupMatches(group_G, i);
-        SimulateGroupMatches(group_H, i);
+        SimulateGroupEliminationPhase(i);
         break;
       case 1:
         console.log("\n\nGrupna faza - II kolo:\n");
-        SimulateGroupMatches(group_A, i);
-        SimulateGroupMatches(group_B, i);
-        SimulateGroupMatches(group_C, i);
-        SimulateGroupMatches(group_D, i);
-        SimulateGroupMatches(group_E, i);
-        SimulateGroupMatches(group_F, i);
-        SimulateGroupMatches(group_G, i);
-        SimulateGroupMatches(group_H, i);
+        SimulateGroupEliminationPhase(i);
         break;
       case 2:
         console.log("\n\nGrupna faza - III kolo:\n");
-        SimulateGroupMatches(group_A, i);
-        SimulateGroupMatches(group_B, i);
-        SimulateGroupMatches(group_C, i);
-        SimulateGroupMatches(group_D, i);
-        SimulateGroupMatches(group_E, i);
-        SimulateGroupMatches(group_F, i);
-        SimulateGroupMatches(group_G, i);
-        SimulateGroupMatches(group_H, i);
+        SimulateGroupEliminationPhase(i);
         break;
     }
   }
@@ -317,6 +154,114 @@ const main = () => {
   // Resetuje golove na nulu da se ne bi u sledecoj fazi takmicenja preneli rezultati od zadnje utakmice
   goals_for_team_1 = 0;
   goals_for_team_2 = 0;
+
+  function CalculateEvaluationPoints(group) {
+    let temp_array = [];
+    for (let i = 0; i < group.length; i++) {
+      group[i].evaluation_points =
+        group[i].points * 16 +
+        (group[i].goals_given - group[i].goals_received) * 2 +
+        group[i].goals_given * 0.25 +
+        group[i].rank * -0.03125;
+      if (group[i].evaluation_points < 0) {
+        group[i].evaluation_points = (group[i].evaluation_points * -1) / 100;
+      }
+      temp_array.unshift(group[i].evaluation_points);
+    }
+    return temp_array;
+  }
+
+  function SortArrayByEvaluationPoints(array) {
+    return array.sort(function (a, b) {
+      return b - a;
+    });
+  }
+
+  function SortGroupByEvaluationPoints(group, sorted_list) {
+    let shifted_group = [];
+    for (let i = 0; i < group.length; i++) {
+      for (let y = 0; y < group.length; y++) {
+        if (sorted_list[i] === group[y].evaluation_points) {
+          shifted_group[i] = group[y];
+        }
+      }
+    }
+    return shifted_group;
+  }
+
+  function FindTeamIndexInGroup(group, array_element) {
+    for (let i = 0; i < group.length; i++) {
+      if (array_element === group[i].evaluation_points) {
+        return group[i];
+      }
+    }
+  }
+
+  function PickGroupWinners(group, sorted_list) {
+    let group_winners = [];
+    group_winners[0] = FindTeamIndexInGroup(group, sorted_list[0]);
+    group_winners[1] = FindTeamIndexInGroup(group, sorted_list[1]);
+    return group_winners;
+  }
+
+  function PrintGroupResults(group) {
+    let goals_given = "";
+    let goals_received = "";
+    for (let i = 0; i < group.length; i++) {
+      // Zbog boljeg formatiranja
+      if (group[i].goals_given < 10) {
+        goals_given = "0" + group[i].goals_given.toString();
+      } else {
+        goals_given = group[i].goals_given;
+      }
+      if (group[i].goals_received < 10) {
+        goals_received = "0" + group[i].goals_received.toString();
+      } else {
+        goals_received = group[i].goals_received;
+      }
+
+      console.log(
+        "\t\t" +
+          (i + 1) +
+          ". " +
+          group[i].name +
+          " (" +
+          group[i].rank +
+          ")\t" +
+          group[i].wins +
+          "  " +
+          group[i].draws +
+          "  " +
+          group[i].loses +
+          "  " +
+          goals_given +
+          ":" +
+          goals_received +
+          "  " +
+          group[i].points
+      );
+    }
+  }
+
+  function RoundUpResults(group, winners_group) {
+    list_of_evaluation_points = CalculateEvaluationPoints(group);
+    sorted_list_of_evaluation_points = SortArrayByEvaluationPoints(list_of_evaluation_points);
+
+    group = SortGroupByEvaluationPoints(group, sorted_list_of_evaluation_points);
+    winners_group = PickGroupWinners(group, sorted_list_of_evaluation_points);
+
+    // Zbog boljeg formatiranja
+    if (group[0].group === "A") {
+      console.log("\n\tGrupa " + group[0].group);
+    } else {
+      console.log("\n\n\tGrupa " + group[0].group);
+    }
+    PrintGroupResults(group);
+
+    winners_group[0].group += "1";
+    winners_group[1].group += "2";
+    return winners_group;
+  }
 
   let group_A_winners = [];
   let group_B_winners = [];
@@ -327,36 +272,67 @@ const main = () => {
   let group_G_winners = [];
   let group_H_winners = [];
 
-  let list_of_eval_points = [];
-  let sorted_list = [];
+  let list_of_evaluation_points = [];
+  let sorted_list_of_evaluation_points = [];
 
   console.log("\n\nKraj grupne faze. Stanje na tabeli po grupama:");
   for (let i = 0; i < 8; i++) {
     switch (i) {
       case 0:
-        group_A_winners = RoundupResults(group_A, group_A_winners);
+        group_A_winners = RoundUpResults(group_A, group_A_winners);
         break;
       case 1:
-        group_B_winners = RoundupResults(group_B, group_B_winners);
+        group_B_winners = RoundUpResults(group_B, group_B_winners);
         break;
       case 2:
-        group_C_winners = RoundupResults(group_C, group_C_winners);
+        group_C_winners = RoundUpResults(group_C, group_C_winners);
         break;
       case 3:
-        group_D_winners = RoundupResults(group_D, group_D_winners);
+        group_D_winners = RoundUpResults(group_D, group_D_winners);
         break;
       case 4:
-        group_E_winners = RoundupResults(group_E, group_E_winners);
+        group_E_winners = RoundUpResults(group_E, group_E_winners);
         break;
       case 5:
-        group_F_winners = RoundupResults(group_F, group_F_winners);
+        group_F_winners = RoundUpResults(group_F, group_F_winners);
         break;
       case 6:
-        group_G_winners = RoundupResults(group_G, group_G_winners);
+        group_G_winners = RoundUpResults(group_G, group_G_winners);
         break;
       case 7:
-        group_H_winners = RoundupResults(group_H, group_H_winners);
+        group_H_winners = RoundUpResults(group_H, group_H_winners);
         break;
+    }
+  }
+
+  function PrintMatchResult(team1, team2) {
+    console.log(
+      "\t(" +
+        team1.group +
+        ") " +
+        team1.name +
+        "\t" +
+        goals_for_team_1 +
+        ":" +
+        goals_for_team_2 +
+        "\t" +
+        team2.name +
+        " (" +
+        team2.group +
+        ")"
+    );
+  }
+
+  function SimulateGame(team1, team2) {
+    do {
+      GetGoals();
+    } while (goals_for_team_1 === goals_for_team_2);
+    PrintMatchResult(team1, team2);
+
+    if (goals_for_team_1 > goals_for_team_2) {
+      return team1;
+    } else {
+      return team2;
     }
   }
 
